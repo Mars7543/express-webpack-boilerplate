ef6c82b5c5b2ba243fe22b148f731f91a395c0bd
Mars7543 <miguelaroberts09@gmail.com>
2019-08-25 11:43:43 -0400

    Modified scripts && Server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 import express from "express";
-import { add } from "./add";
-
-console.log(add(1, 2));
 
 const server = express();
 const port = process.env.PORT || 3000;
 
-// server.listen(port, () => console.log(`Server running on port ${port}...`));
+server.listen(port, () => console.log(`Server running on port ${port}...`));
